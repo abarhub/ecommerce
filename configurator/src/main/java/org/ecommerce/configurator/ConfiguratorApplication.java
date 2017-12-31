c530bcad5ea03ba412f70c7f08d71b81927b84ea
toto963741 <tulipebleu1@laposte.net>
2017-12-31 15:38:54 +0100

    ajout du projet configurator

---
+++ configurator/src/main/java/org/ecommerce/configurator/ConfiguratorApplication.java
@@ -0,0 +1,16 @@
+package org.ecommerce.configurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
+public class ConfiguratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfiguratorApplication.class, args);
+	}
+}
