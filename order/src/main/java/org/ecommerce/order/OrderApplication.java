1c91a2352a2db66103284f592e2dca53be9ceace
abarhub <abarhub@gmail.com>
2023-06-18 16:33:50 +0200

    ajout de prometeus

--- order/src/main/java/org/ecommerce/order/OrderApplication.java
+++ order/src/main/java/org/ecommerce/order/OrderApplication.java
@@ -1,5 +1,7 @@
 package org.ecommerce.order;
 
+import io.prometheus.client.spring.boot.EnablePrometheusEndpoint;
+import io.prometheus.client.spring.boot.EnableSpringBootMetricsCollector;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -10,6 +12,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableDiscoveryClient
 @ComponentScan(basePackages = "org.ecommerce.order")
 @EnableJpaRepositories("org.ecommerce.order.dao")
+@EnablePrometheusEndpoint
+@EnableSpringBootMetricsCollector
 public class OrderApplication {
 
 	public static void main(String[] args) {
